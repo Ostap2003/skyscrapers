10f957639adabcd7c283730e4963c55cd5c8e6a0
Ostap <ostap.dutka@ucu.edu.ua>
2021-02-03 14:21:42 +0200

    added file

--- skyscrapers.py
+++ skyscrapers.py
@@ -32,17 +32,24 @@ def left_to_right_check(input_line: str, pivot: int):
     True
     >>> left_to_right_check("452453*", 5)
     False
+    >>> left_to_right_check('2 41325 1', 2))
+    True
     """
-    cursor = 0
-    for el in input_line[1:-1]:
-        if int(el) > pivot:
-            return True
+    max_el_id = input_line.find(max(input_line[1:-1]))
+    if max_el_id == 1:
+        return 1 == pivot
 
-        else:
-            cursor += 1
-            left_to_right_check(input_line[cursor:], pivot)
+    max_el_inside = max(input_line[1:max_el_id])
+    max_el_inside_id = input_line[1:-1].find(max_el_inside) + 1
+    
+    visible = 0
+    for el in range(len(input_line[1:max_el_id + 1])):
+        if (input_line[el] <= max_el_inside) and (el <= max_el_inside_id):
+            visible += 1
+        elif (input_line[el] > max_el_inside) and (el > max_el_inside_id):
+            visible += 1
 
-    return False
+    return pivot == visible
 
 
 def check_not_finished_board(board: list):
@@ -121,6 +128,7 @@ def check_columns(board: list):
     >>> check_columns(['***21**', '412553*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
     False
     """
+    # check uniqueness
     for row in range(1, len(board[:-1])):
         col_num_entry = set()
         for col in range(1, len(board[:-1])):
@@ -128,6 +136,33 @@ def check_columns(board: list):
                 return False
             else:
                 col_num_entry.add(int(board[col][row]))
+    
+    # check visibility
+    for el in range(len(board)):
+        try:
+            if isinstance(int(board[0][el]), int):
+                top_down = ''.join([board[row][el] for row in range(len(board))])
+                # print('top-down = ', top_down)
+                if left_to_right_check(top_down, int(top_down[0])):
+                    pass
+                else:
+                    return False
+
+        except ValueError:
+            pass
+    
+    for el in range(len(board)):
+        try:
+            if isinstance(int(board[-1][el]), int):
+                bottom_up = ''.join([board[row][el] for row in range(len(board))][::-1]) # reverse to pass to the left_to_right_check
+                # print('bottom_up = ', bottom_up)
+                if left_to_right_check(bottom_up, int(bottom_up[0])):
+                    pass
+                else:
+                    return False
+
+        except ValueError:
+            pass
 
     return True
 
@@ -147,11 +182,15 @@ def check_skyscrapers(input_path: str):
 if __name__ == "__main__":
     # print(read_input('input.txt'))
 
-    # print(left_to_right_check("452453*", 5))
+    # print(left_to_right_check("452453*", 5))  # False
+    # print(left_to_right_check("412453*", 4))  # True
+    # print(left_to_right_check('2413251', 2))  # True
 
     # print(check_not_finished_board(['***21**', '412453*', '423145*', '*503215', '*35214*', '*41532*', '*2*1***']))
 
     # print(check_uniqueness_in_rows(['***21**', '412453*', '423145*', '*553215', '*35214*', '*41532*', '*2*1***']))
 
-    # print(check_columns(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
-    # print(check_columns(['***21**', '412553*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
+
+    # print(check_columns(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))  # True
+    # print(check_columns(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41232*', '*2*1***']))  # False
+    # print(check_columns(['***21**', '412553*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))  # False
